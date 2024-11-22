e3dfce4064846d7526d24ad67eb7d99d43e890f3
SoggyWetCheese <geniusboy93195m@gmail.com>
2024-11-22 22:22:06 +0000

    i translated my book

--- src/App.java
+++ src/App.java
@@ -31,9 +31,9 @@ public class App {
         // input.readFromString("Test", "Dog\nCat\nMouse");
 
         // Example reading from a URL
-        input.readFromUrl("Romeo and Juliette", "https://gutenberg.pglaf.org/cache/epub/1513/pg1513.txt");
+        //input.readFromUrl("Romeo and Juliette", "https://gutenberg.pglaf.org/cache/epub/1513/pg1513.txt");
 
-        //input.readFromUrl("Viennese Medley", "https://www.gutenberg.org/cache/epub/74757/pg74757-images.html");
+        input.readFromUrl("Viennese Medley", "https://www.gutenberg.org/cache/epub/74757/pg74757-images.html");
 
         input.printlines(116, 32);
         Book output = PigLatinTranslator.translate(input);
